54f2f80fabbd94190aa4d69851a7eaa8a4588eea
sachinthaNavindu <navindus485@gmail.com>
2026-08-23 20:54:20 +0530

    Updated submodules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ module.exports = {
       name   : "service-registry",
       script : "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
       log_file: "./logs/service-registry.log"
+
     },
     {
       name   : "api-gateway",
