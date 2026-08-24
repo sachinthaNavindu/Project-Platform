c0d6200eeea755945adcc8f022f4510bcc0c6e36
sachinthaNavindu <navindus485@gmail.com>
2026-08-24 11:19:26 +0530

    Updated submodules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-  apps : [
+  apps: [
     {
-      name   : "config-server",
-      script : "java -jar ./config-server/target/Config-Server-1.0.0.jar",
+      name: "config-server",
+      script: "java",
+      args: "-jar ./config-server/target/Config-Server-1.0.0.jar",
       log_file: "./logs/config-server.log"
     },
     {
-      name   : "service-registry",
-      script : "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
+      name: "service-registry",
+      script: "java",
+      args: "-jar ./service-registry/target/Service-Registry-1.0.0.jar",
       log_file: "./logs/service-registry.log"
-
     },
     {
-      name   : "api-gateway",
-      script : "java -jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
+      name: "api-gateway",
+      script: "java",
+      args: "-jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
       log_file: "./logs/api-gateway.log"
     }
   ]
-}
+};
